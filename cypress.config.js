b7a259cc0021f1d6735485689ab1f9a9b59778a8
Aqib <aqib.mukhtar70@gmail.com>
2024-08-07 18:31:47 +0200

    ReportsUpload

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
     projectId: "ocnw76",
-    video: false,
+    video: true,
     reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
         reportDir: 'cypress/reports',
